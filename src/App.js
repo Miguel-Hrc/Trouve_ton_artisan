59f8d98878c635c3b3b1a1b330ddc1d90b0ed860
Miguel-Hrc <Migu-el_l@outlook.fr>
2024-08-13 21:05:05 +0400

    commit Stylise_Header

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import './App.css';
+import './App.sass';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import Header from './Component/Header';
